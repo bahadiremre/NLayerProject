9850f2c3e20c4428154c994311a4bf5de2af01c1
bahadiremre <bahadiremre2744@gmail.com>
2021-04-17 19:12:38 +0300

    Repositories

---
+++ NLayerProject.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Repositories
+{
+    interface ICategoryRepository : IRepository<Category>
+    {
+        Task<Category> GetWithProductsById(int categoryId);
+    }
+}
